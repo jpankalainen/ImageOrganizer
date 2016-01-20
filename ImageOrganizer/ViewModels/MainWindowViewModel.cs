538734c97295e248ed14a009102b90808104aae7
Jouni Pankalainen <jouni.pankalainen@gmail.com>
2016-01-20 20:23:43 +0200

    Basic design for main window

---
+++ ImageOrganizer/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Prism.Mvvm;
+using System.Collections.ObjectModel;
+
+namespace ImageOrganizer.ViewModels
+{
+    class MainWindowViewModel : BindableBase
+    {
+        ObservableCollection<DirectoryViewModel> SourceDirectories { get; }
+        ObservableCollection<DirectoryViewModel> TargetDirectories { get; }
+    }
+}
---
+++ ImageOrganizer/Views/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using ImageOrganizer.ViewModels;
+using System.Windows;
+
+namespace ImageOrganizer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowViewModel();
+        }
+    }
+}
